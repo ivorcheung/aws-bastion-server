26577c88d22d7273dbe1b9edc9c98821b5c47f28
Ivor Cheung <ivor.cheung@infinityworks.com>
2020-04-21 08:54:10 +0100

    Added more boilerplate code for an availability zone, and an internet gateway

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,43 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "bastion_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "dedicated"
 
   tags = {
-    Name = "main"
+    Name = "bastion"
   }
+}
+
+
+
+##### Internet Gateway
+
+variable "vpc_id" {}
+
+data "aws_internet_gateway" "default" {
+  filter {
+    name   = "attachment.vpc-id"
+    values = ["${var.vpc_id}"]
+  }
+}
+
+
+##### Availability Zones
+
+# Declare the data source
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# e.g. Create subnets in the first two available availability zones
+
+resource "aws_subnet" "primary" {
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  # ...
+}
+
+resource "aws_subnet" "secondary" {
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  # ...
 }
